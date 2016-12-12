7cc67077ec5b7e2bebdb5c10409cdcddd85d8a57
Jnet <jnetmac@no-device-found.local>
2016-12-12 21:26:30 +0530

    Table View Implementation

--- BusinessVisa/TabController/ChecklistController/BV_ChecklistVC.swift
+++ BusinessVisa/TabController/ChecklistController/BV_ChecklistVC.swift
@@ -8,12 +8,41 @@
 
 import UIKit
 
-class BV_ChecklistVC: UIViewController {
+//
+// MARK: - Section Data Structure
+//
+struct Section {
+    var name: String!
+    var items: [String]!
+    var collapsed: Bool!
+    
+    init(name: String, items: [String], collapsed: Bool = false) {
+        self.name = name
+        self.items = items
+        self.collapsed = collapsed
+    }
+}
 
+
+class BV_ChecklistVC: UIViewController, UITableViewDataSource, UITableViewDelegate{
+
+    
+    @IBOutlet weak var table: UITableView!
+    var tableArray = ""
+    var sections = [Section]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let jsonObject = BV_JsonData.sharedInstance.jsonObject
+        print(jsonObject)
+        
+        sections = [
+            Section(name: "Mac", items: ["MacBook", "MacBook Air", "MacBook Pro", "iMac", "Mac Pro", "Mac mini", "Accessories", "OS X El Capitan"]),
+            Section(name: "iPad", items: ["iPad Pro", "iPad Air 2", "iPad mini 4", "Accessories"]),
+            Section(name: "iPhone", items: ["iPhone 6s", "iPhone 6", "iPhone SE", "Accessories"]),
+        ]
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,7 +50,8 @@ class BV_ChecklistVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    
+    
     /*
     // MARK: - Navigation
 
@@ -32,4 +62,54 @@ class BV_ChecklistVC: UIViewController {
     }
     */
 
+
+    
+    
+    // MARK: - UITableViewDataSource Methods
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //return alphNames.count
+        return sections[section].items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as UITableViewCell? ?? UITableViewCell(style: .default, reuseIdentifier: "cell")
+        
+        cell.textLabel?.text = sections[indexPath.section].items[indexPath.row]
+        
+        return cell
+        
+    }
+    
+
+  
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sections[section].name
+    }
+    
+    // MARK: - UITableViewDelegate Methods
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return sections[indexPath.section].collapsed! ? 0 : 44.0
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 44.0
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 1.0
+    }
+
+    
 }
+
+
